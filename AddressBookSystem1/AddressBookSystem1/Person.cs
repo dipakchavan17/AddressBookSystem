79c29cd6b7c66b3d456a120de82a2d4cec4c0d97
Dipak@1432 <chavandipakr17@gmail.com>
2022-01-30 11:58:07 +0530

    contact details created

---
+++ AddressBookSystem1/AddressBookSystem1/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem1
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string[] Addresses { get; set; }
+        public int zip { get; set; }
+        public string email { get; set; }
+        public static List<Person> People = new List<Person>();
+ 
+
+    }
+}
