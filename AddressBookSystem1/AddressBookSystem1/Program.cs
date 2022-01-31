5d96ffed0846523a1fe6672b67818c244fb574e6
Dipak@1432 <chavandipakr17@gmail.com>
2022-01-31 13:20:47 +0530

    Edit is added in contact

--- AddressBookSystem1/AddressBookSystem1/Program.cs
+++ AddressBookSystem1/AddressBookSystem1/Program.cs
@@ -11,12 +11,52 @@ namespace AddressBookSystem1
         //Main  Method / program Entry Point
         static void Main(string[] args)
         {
-            //Showing Welcome Message On console.
-            Console.WriteLine("Welcome to Address Book Progam \n");
-
-            CreateAddressBook addressBook = new CreateAddressBook();
-            addressBook.AddContact();
-            Console.ReadLine();
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook newaddressbook = new AddressBook();
+            Contact newcontact = new Contact();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Number to Execute the Address book Program \n1. Create contacts \n2. Add contact \n3. Edit contact \n4. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact Information first name, last names, address,city, state, zip, phone number and email id");
+                        newcontact.firstname = Console.ReadLine();
+                        newcontact.lastname = Console.ReadLine();
+                        newcontact.address = Console.ReadLine();
+                        newcontact.city = Console.ReadLine();
+                        newcontact.state = Console.ReadLine();
+                        newcontact.zip = Console.ReadLine();
+                        newcontact.phonenumber = Console.ReadLine();
+                        newcontact.emailid = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Contact Information first name, last names, address,city, state, zip, phone number and email id");
+                        newcontact.firstname = Console.ReadLine();
+                        newcontact.lastname = Console.ReadLine();
+                        newcontact.address = Console.ReadLine();
+                        newcontact.city = Console.ReadLine();
+                        newcontact.state = Console.ReadLine();
+                        newcontact.zip = Console.ReadLine();
+                        newcontact.phonenumber = Console.ReadLine();
+                        newcontact.emailid = Console.ReadLine();
+                        newaddressbook.AddContact(newcontact);
+                        newaddressbook.Display();
+                        break;
+                    case 3:
+                        newaddressbook.Editexistingcontact();
+                        newaddressbook.Display();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
         }
     }
 }
