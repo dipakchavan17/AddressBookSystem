36ce0ed863d87b0647aecce7be60a1eebdcdfdea
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-16 00:08:20 +0530

    [dipak]Add Multiple person

---
+++ AddressBookSystem1/AddressBookSystem1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Showing Welcome Message On console.
+            Console.WriteLine("Welcome to Address Book Progam \n");
+            CreateAddressBook createAddressBook = new CreateAddressBook();
+            createAddressBook.ReadInput();
+            Console.ReadLine();
+        }
+    }
+}
